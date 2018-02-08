ed71ab5c8e315600d2cded013ea88c32b59308b4
Mahmoud Ali <mahmoud.ali@lambda3.com.br>
2018-02-08 19:07:24 -0200

    WIP

---
+++ APM-Start/src/app/products/products.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent {
+}
